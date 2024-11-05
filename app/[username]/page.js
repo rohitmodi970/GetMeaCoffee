ab9cf33ada70c5ee1fd1d74f3460e74cbad2c6a3
ROHIT MODI <132802604+rohitmodi970@users.noreply.github.com>
2024-11-06 03:50:50 +0530

    Update page.js

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,30 +1,48 @@
-import React from 'react'
-import PaymentPage from '@/components/PaymentPage'
-import { notFound } from "next/navigation"
-import User from '@/models/User'
-import connectDB from '@/db/connectDb'
+import React from 'react';
+import PaymentPage from '@/components/PaymentPage';
+import { notFound } from 'next/navigation';
+import User from '@/models/User';
+import connectDB from '@/db/connectDb';
+
 const Username = async ({ params }) => {
-  // if the username not prsent in the datbase , show a 404 page
-  const checkUser=async()=>{
-    await connectDB()
-    let u = await User.findOne({username: params.username})
-    // if (!u) {
-    //   return notFound()
-    // }
-  }
-  console.log(params.username)
-  await checkUser()
+  // Connect to the database and check if the username exists
+  const checkUser = async () => {
+    await connectDB();
+    let user = await User.findOne({ username: params.username });
+    if (!user) {
+      return notFound(); // Show a 404 page if user is not found
+    }
+  };
+
+  console.log(params.username);
+  await checkUser();
+
   return (
     <>
-      <PaymentPage  username = {params.username}/>
+      <PaymentPage username={params.username} />
     </>
-  )
-}
+  );
+};
 
-export default Username
+export default Username;
 
+// Generate metadata for SEO
 export async function generateMetadata({ params }) {
   return {
     title: `Support ${params.username} - Get Me a Coffee`,
-  }
+  };
+}
+
+// Generate static params for static export
+export async function generateStaticParams() {
+  // Connect to the database
+  await connectDB();
+  
+  // Fetch all usernames from the database
+  const users = await User.find({}, 'username'); // Only fetch the username field
+  
+  // Map usernames to the format required for static paths
+  return users.map(user => ({
+    username: user.username,
+  }));
 }
